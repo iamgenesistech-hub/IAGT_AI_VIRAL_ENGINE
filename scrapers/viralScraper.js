fbc8c776c3caee128938365fd6c4e9298a863d0b
iamgenesistech-hub <iamgenesistech@gmail.com>
2026-05-09 20:40:00 -0400

    Build experiment governor engine

--- scrapers/viralScraper.js
+++ scrapers/viralScraper.js
@@ -1,32 +1,33 @@
 require('dotenv').config();
 
 const {
-  calculateRenderGrade,
-  determineRenderStatus,
-  shouldEnterEliteVault
-} = require('../utils/renderGradingEngine');
+  enforceTopCreativeCap,
+  selectEliteTop20,
+  shouldMoveToBestOfBest
+} = require('../utils/libraryStewardEngine');
 
 function testSystem() {
-  console.log("EVICS Render Grading Engine Initialized...");
+  console.log("EVICS Library Steward Initialized...");
 
-  const renderScores = {
-    viralPotential: 96,
-    conversionPotential: 94,
-    brandAlignment: 98,
-    productFit: 97,
-    visualQuality: 93,
-    hookStrength: 95,
-    emotionalImpact: 94
-  };
+  const creatives = [
+    { name: "Ad 1", performanceScore: 95 },
+    { name: "Ad 2", performanceScore: 89 },
+    { name: "Ad 3", performanceScore: 92 },
+    { name: "Ad 4", performanceScore: 70 },
+    { name: "Ad 5", performanceScore: 85 },
+    { name: "Ad 6", performanceScore: 60 },
+    { name: "Ad 7", performanceScore: 98 }
+  ];
 
-  const finalGrade = calculateRenderGrade(renderScores);
-  const renderStatus = determineRenderStatus(finalGrade);
-  const eliteVaultDecision = shouldEnterEliteVault(finalGrade, 12);
+  const library = enforceTopCreativeCap(creatives, 5);
+  const eliteTop20 = selectEliteTop20(creatives);
+  const bestOfBest = shouldMoveToBestOfBest(95, 1750);
 
-  console.log("Final Render Grade:", finalGrade);
-  console.log("Render Status:", renderStatus);
-  console.log("Elite Vault Approved:", eliteVaultDecision);
-  console.log("Render Grading Engine Operational");
+  console.log("Active Creatives:", library.activeCreatives.map(c => c.name));
+  console.log("Fallout Creatives:", library.falloutCreatives.map(c => c.name));
+  console.log("Elite Top Count:", eliteTop20.length);
+  console.log("Move To Best Of Best:", bestOfBest);
+  console.log("Library Steward Engine Operational");
 }
 
 testSystem();
